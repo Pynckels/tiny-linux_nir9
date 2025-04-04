da7e42fa831d313d7c5ce57e6db473f38ede80fe
Pynckels <filip.pynckels@telenet.be>
2025-04-04 13:34:39 +0200

    Remove no-stackprotector

--- lnx/very-minimal-shell/build.sh
+++ lnx/very-minimal-shell/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-gcc -c -Os -Wall -Werror -fno-ident -fno-asynchronous-unwind-tables -fno-stack-protector -fomit-frame-pointer -o shell.o shell.c
+gcc -c -Os -Wall -Werror -fno-ident -fno-asynchronous-unwind-tables -fomit-frame-pointer -o shell.o shell.c
 as  -o sys.o sys.S
 ld --strip-all -z noexecstack -z nodefaultlib -T custom.ld -o init shell.o sys.o
